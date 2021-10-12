34e1b115c11dc4d24ab3c1d557b7ff5238854869
fikriachmadi <fikriachmadi>
2021-10-12 15:55:17 +0700

    First Commit

---
+++ JavaBasic.java
@@ -0,0 +1,15 @@
+public class JavaBasic {
+    public static void main(String[] args){
+        String firstname = "Fikri"; 
+        String lastname = "Achmadi";
+        int age = 21;
+        String address = "Lampung";
+        int phone = 156136438;
+        
+        System.out.println("First Name : " + firstname);
+        System.out.println("Last Name : " + lastname);
+        System.out.println("Age : " + age);
+        System.out.println("Address : " + address);
+        System.out.println("Phone Number : " + phone);
+    }
+}
